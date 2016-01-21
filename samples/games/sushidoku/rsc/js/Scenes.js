9a9f8b7be7c2349055847a64fa0aad5cc7da1a22
ivanixgames <ivanixcu@IvanixVIAO>
2016-01-21 15:25:11 +0800

    snapshot of samples with sushidoku game

---
+++ samples/games/sushidoku/rsc/js/Scenes.js
@@ -0,0 +1,15 @@
+/*global
+ Phaser: true,
+ Sudoku: true,
+ console: true
+*/
+//	"use strict";
+
+var g = {};
+var Scene = {};
+
+/*
+Scene.Menu = {};
+Scene.Menu.Main = function (game) { "use strict"; };
+Scene.Play = function (game) { "use strict"; };
+*/
